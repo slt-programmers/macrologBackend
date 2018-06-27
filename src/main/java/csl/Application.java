21789326680504683c7c3761b09a4925c8d41b10
Arjan Tienkamp <arjan.tienkamp@gmail.com>
2018-06-27 15:06:18 +0200

    logging en automatisch reinstall database

--- src/main/java/csl/Application.java
+++ src/main/java/csl/Application.java
@@ -1,15 +1,77 @@
 package csl;
 
+import csl.database.DatabaseHelper;
+import csl.database.FoodRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+        boolean automaticResetDatabase = true;
 
+        boolean tableExists = isDatabaseOpgezet();
+
+        if (tableExists && automaticResetDatabase){
+            deleteTables();
+        }
+        if (!tableExists || automaticResetDatabase) {
+            createTables();
+        }
+
+    }
+
+    private static void deleteTables() {
+        try {
+            Connection connection = DatabaseHelper.getInstance().getConnection();
+            CallableStatement callableStatement = connection.prepareCall(FoodRepository.TABLE_DELETE);
+            boolean execute = callableStatement.execute();
+            LOGGER.debug("Succes REMOVE: " + execute);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createTables() {
+        try {
+            Connection connection = DatabaseHelper.getInstance().getConnection();
+            CallableStatement callableStatement = connection.prepareCall(FoodRepository.TABLE_CREATE);
+            boolean execute = callableStatement.execute();
+            LOGGER.debug("Succes INSTALL: " + execute);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static boolean isDatabaseOpgezet() throws SQLException {
+        NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(new JdbcTemplate(DatabaseHelper.getInstance()));
+
+        String checkUrl = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where table_name = '" + FoodRepository.TABLE_NAME + "'";
+        List<String> results = template.query(checkUrl, new RowMapper<String>() {
+            @Override
+            public String mapRow(ResultSet resultSet, int i) throws SQLException {
+                return resultSet.getString("TABLE_NAME");
+            }
+        });
+        return results.size() == 1;
     }
 }
--- src/main/java/csl/database/FoodRepository.java
+++ src/main/java/csl/database/FoodRepository.java
@@ -1,19 +1,16 @@
 package csl.database;
 
 import csl.database.model.Food;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Logger;
 
 import static csl.database.FoodRepository.*;
 
@@ -40,7 +37,7 @@ public class FoodRepository {
                     COL_FATS + " DEC(5,2) NOT NULL, " +
                     COL_CARBS + " DEC(5,2) NOT NULL," +
                     COL_DEFAULT_AMOUNT + " DEC(5,2) NOT NULL," +
-                    COL_DEFAULT_AMOUNT_UNIT + " TEXT NOT NULL)" ;
+                    COL_DEFAULT_AMOUNT_UNIT + " TEXT NOT NULL)";
 
     public static final String TABLE_DELETE =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -53,27 +50,6 @@ public class FoodRepository {
     public FoodRepository() {
 
 
-        String checkUrl = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where table_name = '"+TABLE_NAME+"'";
-
-        List<String> results = template.query(checkUrl, new RowMapper<String>() {
-            @Override
-            public String mapRow(ResultSet resultSet, int i) throws SQLException {
-                return resultSet.getString("TABLE_NAME");
-            }
-        });
-        if (results.size() == 0){
-            System.out.println("Create table");
-            try {
-                Connection connection = DatabaseHelper.getInstance().getConnection();
-                CallableStatement callableStatement = connection.prepareCall(TABLE_CREATE);
-                boolean execute = callableStatement.execute();
-                System.out.println("Succes installatie: " + execute);
-           } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-
     }
 
     public List<Food> getAllFood() {
@@ -82,7 +58,7 @@ public class FoodRepository {
 
     public int insertFood(Food food) {
         SqlParameterSource params = new MapSqlParameterSource()
-               .addValue("id", null)
+                .addValue("id", null)
                 .addValue("name", food.getName())
                 .addValue("amount", food.getAmountNumber())
                 .addValue("unitname", food.getAmountUnit())
@@ -95,7 +71,7 @@ public class FoodRepository {
     public List<Food> getFood(String name) {
         SqlParameterSource params = new MapSqlParameterSource()
                 .addValue("name", name);
-        String myFood = SELECT_SQL + " WHERE  "+ COL_NAME + "= :name";
+        String myFood = SELECT_SQL + " WHERE  " + COL_NAME + "= :name";
         List<Food> queryResults = template.query(myFood, params, new FoodWrapper());
         return queryResults;
     }
@@ -111,6 +87,6 @@ class FoodWrapper implements RowMapper {
                 rs.getDouble(COL_PROTEINS),
                 rs.getDouble(COL_FATS),
                 rs.getDouble(COL_CARBS)
-                );
+        );
     }
 }
