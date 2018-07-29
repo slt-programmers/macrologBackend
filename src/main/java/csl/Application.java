2cc76442b6f00d834ed94b36c0db8a526d5005de
Arjan Tienkamp <arjan.tienkamp@gmail.com>
2018-07-29 09:30:05 +0200

    jwt token en userrepository

--- src/main/java/csl/Application.java
+++ src/main/java/csl/Application.java
@@ -16,10 +16,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+//import csl.security.SecurityFilter;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class Application {
+
+
+//    @Bean
+//    public FilterRegistrationBean<SecurityFilter> loggingFilter() {
+//        FilterRegistrationBean<SecurityFilter> registrationBean
+//                = new FilterRegistrationBean<>();
+//
+//        registrationBean.setFilter(new SecurityFilter());
+//        registrationBean.addUrlPatterns("/logs/*");
+//
+//        return registrationBean;
+//    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
@@ -28,6 +42,11 @@ public class Application {
 
         //DEV/TEST purposes
         boolean clearTablesOnStartup = false;
+        boolean doUpgrade = false;
+
+        if (doUpgrade) {
+            upgrade();
+        }
 
         if (clearTablesOnStartup) {
             deleteTables();
@@ -52,8 +71,9 @@ public class Application {
              CallableStatement deleteOldFoodAlias = connection.prepareCall("DROP TABLE IF EXISTS FOOD_ALIAS");
              CallableStatement deleteLogEntry = connection.prepareCall(LogEntryRepository.TABLE_DELETE);
              CallableStatement deletePortion = connection.prepareCall(PortionRepository.TABLE_DELETE);
-             CallableStatement deleteFood = connection.prepareCall(FoodRepository.TABLE_DELETE)) {
-            CallableStatement deleteSettings = connection.prepareCall(SettingsRepository.TABLE_DELETE);
+             CallableStatement deleteFood = connection.prepareCall(FoodRepository.TABLE_DELETE);
+             CallableStatement deleteSettings = connection.prepareCall(SettingsRepository.TABLE_DELETE);
+             CallableStatement deleteUser = connection.prepareCall(UserAcccountRepository.TABLE_DELETE)) {
 
             LOGGER.info("Deleting old food alias");
             deleteOldFoodAlias.execute();
@@ -65,20 +85,38 @@ public class Application {
             deleteFood.execute();
             LOGGER.info("Deleting deleteLogEntry");
             deleteSettings.execute();
+            LOGGER.info("Deleting deleteUser");
+            deleteUser.execute();
 
         } catch (SQLException e) {
             LOGGER.error(e.getMessage());
         }
     }
 
+    private static void upgrade() {
+        try (Connection connection = DatabaseHelper.getInstance().getConnection();
+             CallableStatement userTableCreate = connection.prepareCall(UserAcccountRepository.TABLE_CREATE)) {
+            userTableCreate.execute();
+        } catch (
+                SQLException e)
+
+        {
+            LOGGER.error("Failed to upgrade");
+            LOGGER.error(e.getMessage());
+        }
+
+    }
+
     private static void createTables() {
         LOGGER.info("Creating tables on startup");
         try (Connection connection = DatabaseHelper.getInstance().getConnection();
+             CallableStatement userTableCreate = connection.prepareCall(UserAcccountRepository.TABLE_CREATE);
              CallableStatement settingsTableCreate = connection.prepareCall(SettingsRepository.TABLE_CREATE);
              CallableStatement footTableCreate = connection.prepareCall(FoodRepository.TABLE_CREATE);
              CallableStatement foodPortionCreate = connection.prepareCall(PortionRepository.TABLE_CREATE);
              CallableStatement logEntryTableCreate = connection.prepareCall(LogEntryRepository.TABLE_CREATE)) {
 
+            userTableCreate.execute();
             settingsTableCreate.execute();
             footTableCreate.execute();
             foodPortionCreate.execute();
@@ -93,17 +131,18 @@ public class Application {
 
     private static boolean isDatabaseSetUp() throws SQLException {
         NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(new JdbcTemplate(DatabaseHelper.getInstance()));
-        String checkUrl = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where" +
+        String checkUrl = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where (" +
                 " table_name = '" + SettingsRepository.TABLE_NAME + "' or " +
                 " table_name = '" + FoodRepository.TABLE_NAME + "' or " +
                 " table_name = '" + PortionRepository.TABLE_NAME + "' or " +
-                " table_name = '" + LogEntryRepository.TABLE_NAME + "'";
+                " table_name = '" + UserAcccountRepository.TABLE_NAME + "' or " +
+                " table_name = '" + LogEntryRepository.TABLE_NAME + "')";
         List<String> results = template.query(checkUrl, new RowMapper<String>() {
             @Override
             public String mapRow(ResultSet resultSet, int i) throws SQLException {
                 return resultSet.getString("TABLE_NAME");
             }
         });
-        return results.size() == 4;
+        return results.size() == 5;
     }
 }
--- src/main/java/csl/rest/AuthenticationService.java
+++ src/main/java/csl/rest/AuthenticationService.java
@@ -1,31 +1,26 @@
 package csl.rest;
 
-import csl.database.FoodRepository;
-import csl.database.LogEntryRepository;
-import csl.database.PortionRepository;
-import csl.database.SettingsRepository;
-import csl.database.model.Food;
-import csl.database.model.Setting;
-import csl.dto.*;
+import csl.database.UserAcccountRepository;
+import csl.database.model.UserAccount;
+import csl.dto.AuthenticationRequest;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Date;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static csl.security.SecurityConstants.EXPIRATION_TIME;
+import static csl.security.SecurityConstants.SECRET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -33,34 +28,55 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 public class AuthenticationService {
 
 
+    private final static UserAcccountRepository USER_ACCCOUNT_REPOSITORY = new UserAcccountRepository();
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationService.class);
 
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/authenticate",
             method = POST,
             headers = {"Content-Type=application/json"})
-    public ResponseEntity addFood(@RequestBody AuthenticationRequest request) {
+    public ResponseEntity authenticateUser(@RequestBody AuthenticationRequest request) {
         LOGGER.error("Login attempt:" + request.getUsername() + " - " + request.getPassword());
 
+        UserAccount userAccount = USER_ACCCOUNT_REPOSITORY.getUser(request.getUsername());
+        if (userAccount == null || !userAccount.getPassword().equals(request.getPassword())) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        } else {
 
-        try {
-            String jwt = Jwts.builder()
-                    .setSubject("users/TzMUocMF4p")
-                   // .setExpiration(new Date(1300819380))
-                    .claim("name", "Macro Log")
-                    .claim("scope", "self groups/admins")
-                    .signWith(
-                            SignatureAlgorithm.HS256,
-                            "secret".getBytes("UTF-8")
-                    )
-                    .compact();
-            MultiValueMap<String, String> responseHeaders = new HttpHeaders();
-            responseHeaders.add("token",jwt);
-            return new ResponseEntity("200",responseHeaders, HttpStatus.ACCEPTED);
+            try {
+                String jwt = Jwts.builder()
+                        .setSubject("users/TzMUocMF4p")
+                        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+//                        .claim("name", "Macro Log")
+                        .claim("userId", userAccount.getId())
+//                        .claim("scope", "self groups/admins")
+                        .signWith(
+                                SignatureAlgorithm.HS256,
+                                SECRET.getBytes("UTF-8")
+                        )
+                        .compact();
+                MultiValueMap<String, String> responseHeaders = new HttpHeaders();
+                responseHeaders.add("token", jwt);
+                return new ResponseEntity("{\"token\":\"" + jwt + "\"}", responseHeaders, HttpStatus.ACCEPTED);
 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+
+            return ResponseEntity.ok("ok");
         }
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @RequestMapping(value = "/signup",
+            method = POST,
+            headers = {"Content-Type=application/json"})
+    public ResponseEntity signUp(@RequestBody AuthenticationRequest request) {
+        LOGGER.error("Add user attempt:" + request.getUsername() + " - " + request.getPassword());
+
+        String encodedPassword = request.getPassword(); // todo = encode
+        USER_ACCCOUNT_REPOSITORY.insertUser(request.getUsername(), encodedPassword);
 
         return ResponseEntity.ok("ok");
 
